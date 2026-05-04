240c5264d854b1da6cd10cca24cb2af39ca79d92
Jordan Chase <143756329+jordanchase@users.noreply.github.com>
2026-05-04 16:09:31 -0700

    Update astro.config.mjs

--- homepage-site/astro.config.mjs
+++ homepage-site/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jordanchase.github.io/lmu-cmsi-3802-homework1/',
+    site: 'https://jordanchase.github.io/rimere/',
     base: '/rimere>',
 });
