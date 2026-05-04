09aaba2d70fc827c68276c0f0e02fd4737a8b3d8
awhitneymiller <awhitneymiller0@gmail.com>
2026-05-04 12:35:42 -0700

    updated astro config

--- homepage-site/astro.config.mjs
+++ homepage-site/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jordanchase.github.io/lmu-cmsi-3802-homework1/'
+    site: 'https://jordanchase.github.io/lmu-cmsi-3802-homework1/',
+    base: '/lmu-cmsi-3802-homework1>',
 });
