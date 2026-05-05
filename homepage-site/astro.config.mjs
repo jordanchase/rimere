d0272009219be541259b079dd555ae6f48c7e8a0
awhitneymiller <awhitneymiller0@gmail.com>
2026-05-04 22:53:12 -0700

    hopefully fixed images?

--- homepage-site/astro.config.mjs
+++ homepage-site/astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+<<<<<<< HEAD
   site: 'https://jordanchase.github.io',
   base: '/rimere',
 })
+=======
+  site: 'https://jordanchase.github.io/lmu-cmsi-3802-homework1/',
+  base: '/lmu-cmsi-3802-homework1',
+});
+>>>>>>> d316301 (hopefully fixed images?)
